d97b37a3f7e5a45e9efc4a0ece77465b1518b173
giwan <giwan.dev@gmail.com>
2022-11-01 09:14:28 +0900

    ✨ Node.js는 nvm으로 관리하도록 변경
    Brewfile에서 node를 제거합니다.
    init 스크립트에서 nvm 설치 커맨드를 실행하고,
    .zshrc에 nvm 설정 코드를 추가합니다.

--- home/.zshrc.sh
+++ home/.zshrc.sh
@@ -18,6 +18,11 @@ fi
 # Register directory
 hash -d dev="$dev"
 
+# nvm configuration
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
 if [[ "$OSTYPE" == darwin* ]]; then
   eval "$(starship init zsh)"
 fi
--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 
+echo "Install nvm..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+
 source ./scripts/symlink-dotfiles.sh
 
 if [[ "$OSTYPE" == darwin* ]]; then
