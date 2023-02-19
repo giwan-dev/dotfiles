e6acc9c111f871c79483fe8247c9e23e27e5a377
giwan <giwan.dev@gmail.com>
2023-02-19 22:15:51 +0900

    .zprofile을 동기화
    중복된 nvm 셋업을 .zshrc에서 제거합니다.

--- home/.zshrc.sh
+++ home/.zshrc.sh
@@ -14,11 +14,6 @@ alias g="git"
 # Register directory
 hash -d dev="$dev"
 
-# nvm configuration
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-
 if [[ "$OSTYPE" == darwin* ]]; then
   eval "$(starship init zsh)"
 fi
