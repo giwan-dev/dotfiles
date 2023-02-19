6fc53e454c81d56a9159cb03c2461c1b6a795377
giwan <giwan.dev@gmail.com>
2023-02-19 23:21:38 +0900

    불필요한 echo 제거

--- scripts/install-brew-bundles.sh
+++ scripts/install-brew-bundles.sh
@@ -4,7 +4,6 @@ brewfile_container="$HOME/dotfiles/Brewfiles"
 
 for brewfile in $(find $brewfile_container -name '*.rb'); do
   file_name="${brewfile##*/}"
-  echo $file_name
 
   echo "$file_name 번들을 설치하려 합니다. 설치하시겠습니까? (y/N)"
   read yes
