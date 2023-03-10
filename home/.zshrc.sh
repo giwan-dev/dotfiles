4e5acf60e400dab4c421281ce3ff9ba7dcad486a
giwan <giwan.dev@gmail.com>
2023-03-10 11:34:10 +0900

    포트 넘버로 프로세스 종료하는 명령어 추가

--- home/.zshrc.sh
+++ home/.zshrc.sh
@@ -28,3 +28,15 @@ alias l="exa --header --long --all"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
+# 입력한 포트를 사용할 수 있게 사용 중인 프로세스를 종료하는 명령어
+function yield_port {
+  port=$1
+  pid=$(lsof -i:$port | grep "LISTEN" | awk '{ print $2 }')
+  if [[ -z $pid ]]; then
+    echo "Port not in use"
+  else
+    kill $pid
+    echo "Process using port $port has been terminated"
+  fi
+}
