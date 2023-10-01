101d03f03506027e0aa4201bbd78dc7b55773641
giwan <giwan.dev@gmail.com>
2023-10-01 19:08:08 +0900

    선언전 참조 오류 수정

--- scripts/check-git-diff.js
+++ scripts/check-git-diff.js
@@ -7,6 +7,32 @@
 
 const { exec } = require("child_process");
 
+const reset = "\x1b[0m";
+const bright = "\x1b[1m";
+const dim = "\x1b[2m";
+const underscore = "\x1b[4m";
+const blink = "\x1b[5m";
+const reverse = "\x1b[7m";
+const hidden = "\x1b[8m";
+
+const black = "\x1b[30m";
+const red = "\x1b[31m";
+const green = "\x1b[32m";
+const yellow = "\x1b[33m";
+const blue = "\x1b[34m";
+const magenta = "\x1b[35m";
+const cyan = "\x1b[36m";
+const white = "\x1b[37m";
+
+const BGblack = "\x1b[40m";
+const BGred = "\x1b[41m";
+const BGgreen = "\x1b[42m";
+const BGyellow = "\x1b[43m";
+const BGblue = "\x1b[44m";
+const BGmagenta = "\x1b[45m";
+const BGcyan = "\x1b[46m";
+const BGwhite = "\x1b[47m";
+
 // TODO: glob으로 설정할 수 있도록 확장하기
 const IGNORING_FILES = ["package-lock.json"];
 
@@ -129,32 +155,6 @@ async function gitGitDiffByFiles({ baseBranch, debug }) {
 function checkIgnoringFile(file) {
   return IGNORING_FILES.includes(file);
 }
-
-const reset = "\x1b[0m";
-const bright = "\x1b[1m";
-const dim = "\x1b[2m";
-const underscore = "\x1b[4m";
-const blink = "\x1b[5m";
-const reverse = "\x1b[7m";
-const hidden = "\x1b[8m";
-
-const black = "\x1b[30m";
-const red = "\x1b[31m";
-const green = "\x1b[32m";
-const yellow = "\x1b[33m";
-const blue = "\x1b[34m";
-const magenta = "\x1b[35m";
-const cyan = "\x1b[36m";
-const white = "\x1b[37m";
-
-const BGblack = "\x1b[40m";
-const BGred = "\x1b[41m";
-const BGgreen = "\x1b[42m";
-const BGyellow = "\x1b[43m";
-const BGblue = "\x1b[44m";
-const BGmagenta = "\x1b[45m";
-const BGcyan = "\x1b[46m";
-const BGwhite = "\x1b[47m";
 
 function logError(message) {
   console.error(`${red}${message}${reset}`);
